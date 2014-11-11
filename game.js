f9e167b87813e6f0a250e0f508373f73526a89fe
Ryan Rendeiro <ryan.rendeiro1@marist.edu>
2014-11-11 15:20:45 -0500

    first change

--- game.js
+++ game.js
@@ -1,45 +1,13 @@
-//Lab 9
-//Ryan Rendeiro
-var inputBox = document.querySelector('#action');
-var text = document.querySelector('#descrip');
-var preformAction = function (action) {
-    switch (action) {
-    case 'Dance':
-        text.innerHTML='We can dance if we want to, we can leave your friends behind. Cause if your friends don\'t dance and if they don\'t then they are no friends of mine.'
-        break;
-    case 'Scream':
-    
-        break;  
-    case 'Run': 
-        break;
-    case 'Hide': 
-        break;
-    case 'Give up': 
-        break;
-    case 'Cry':
-        break;
-    case 'Sleep':
-        break;
-    case 'Eat': 
-        break;
-    case 'Find':  
-        break;
-    case 'Pickup':  
-        break;
-    case 'Go North': 
-        break;
-    case 'Go East':
-        break;
-    case 'Go West':
-        break;
-    case 'Go South': 
-        break;
-    case default:
-        'can\'t do that'
-    }
-}        
-var title = document.querySelector('header > h1');
-title.innerHTML = 'Confusion';
-preformAction(inputBox);
-var button = document.getElementById('runner');
-button.onclick = run;
+var player = ( items[] );
+player.pickup = function(item) {
+  this.items.push(item)
+}
+var interpret = function(input){  
+  var string = input.trim();
+  var result = result.split(' ');
+  return result;
+}
+var execute = function(array) {
+  var action = array.splice(0,0,'');
+  player.pickup(action);
+}  
