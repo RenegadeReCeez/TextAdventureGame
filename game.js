489476bc2ae36d35d28067cc1c6b70cdec1acf73
ryan rendeiro <ryan.rendeiro1@marist.edu>
2014-11-18 10:22:50 -0500

    this should work

--- game.js
+++ game.js
@@ -11,3 +11,22 @@ var execute = function(array) {
   var action = array.splice(0,0,'');
   player.pickup(action);
 }  
+var report = function() {
+	inventory = document.querySelector("#inventory > ul");
+	for(var i; i <= player.items.length; i++) {
+		inventory.innerHTML= inventory.innerHTML + player.items[i];
+	}
+}
+var gameStep = function(string) {
+	interpret(string);
+	execute(string);
+	report();
+} 
+var input = document.querySelector("#action");
+input.addEventListener(keyUp, function(event) {
+	if (event.keyCode == 13) {
+		var cmd = interpret(this.value);
+		execute(cmd);
+		report();
+	}
+});
